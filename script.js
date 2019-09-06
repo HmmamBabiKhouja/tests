229a7ab33607a2d3138f241fbb94f2ebedc3b7d1
Hmmambk1994 <bhmmam@gmail.com>
2019-09-06 05:47:41 +0200

    adding the js code to get the width and dpr of screen

---
+++ script.js
@@ -0,0 +1,19 @@
+function showWidth() {
+  alert(
+    window.screen.width * window.devicePixelRatio +
+      " " +
+      window.screen.width +
+      "  " +
+      window.devicePixelRatio
+  );
+}
+
+function showHeight() {
+  alert(
+    window.screen.height * window.devicePixelRatio +
+      " " +
+      window.screen.height +
+      "  " +
+      window.devicePixelRatio
+  );
+}
