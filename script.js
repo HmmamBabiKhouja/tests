1d4fd91151bdf157c38fa1a380013693f2279e82
Hmmambk1994 <bhmmam@gmail.com>
2019-10-17 22:10:37 +0200

    the final edit

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-function showWidth() {
-  alert(
+document.querySelector(".width-height").innerHTML=window.screen.width+"/"+window.screen.height;
+document.querySelector(".retina").innerHTML=window.devicePixelRatio;
+document.querySelector(".resolution").innerHTML=
     window.screen.width * window.devicePixelRatio +
-      " " +
-      window.screen.width +
-      "  " +
-      window.devicePixelRatio
-  );
-}
-
-function showHeight() {
-  alert(
-    window.screen.height * window.devicePixelRatio +
-      " " +
-      window.screen.height +
-      "  " +
-      window.devicePixelRatio
-  );
-}
+      "/" +
+      window.screen.height * window.devicePixelRatio;
